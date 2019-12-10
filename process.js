eef99381e19abf93bcb37a086573db907a9cb70b
stanislav7766 <stanislavtihonov5@gmail.com>
2019-12-10 14:58:24 +0200

    js realization

---
+++ process.js
@@ -0,0 +1,57 @@
+'use strict';
+
+const crypto = require('crypto'),
+  fs = require('fs');
+
+const separateInputsFiles = inputFiles =>
+  inputFiles.map(fileName => fileName.split('.')[0]);
+
+const getHash = (filePath, algorithm = 'md5') =>
+  new Promise((resolve, reject) => {
+    const md5Sum = crypto.createHash(algorithm);
+    try {
+      const rs = fs.ReadStream(filePath);
+      rs.on('data', data => md5Sum.update(data));
+      rs.on('end', () => resolve(md5Sum.digest('hex')));
+      rs.on('error', err => reject(err));
+    } catch (err) {
+      return reject(err);
+    }
+  });
+
+const writeFile = (filePath, data) =>
+  new Promise((resolve, reject) => {
+    try {
+      fs.writeFile(filePath, data, err =>
+        err ? reject(err) : resolve(filePath)
+      );
+    } catch (err) {
+      reject(err);
+    }
+  });
+const listDir = dirName =>
+  new Promise((resolve, reject) => {
+    try {
+      fs.readdir(dirName, (err, files) => (err ? reject(err) : resolve(files)));
+    } catch (err) {
+      reject(err);
+    }
+  });
+
+(async function main() {
+  if (process.argv.length <= 2) throw new Error('must be addition args');
+  const format = {
+    txt: '.txt',
+    res: '.res'
+  };
+  const [dirInput, dirOutput] = process.argv.slice(2);
+  const inputFiles = separateInputsFiles(await listDir(dirInput));
+  const hashPromises = inputFiles.map(fileName =>
+    getHash(dirInput + fileName + format.txt)
+  );
+  const hashFiles = await Promise.all(hashPromises);
+  const writePromises = hashFiles.map((hash, i) =>
+    writeFile(dirOutput + inputFiles[i] + format.res, hash)
+  );
+  await Promise.all(writePromises);
+})();
